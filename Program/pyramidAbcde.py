2599aff620206336e1a3b0aa1bb3657763775db7
PRATYA PALEE <pratya.palee@gmail.com>
2023-02-11 18:11:08 +0700

    Final

--- Program/pyramidAbcde.py
+++ Program/pyramidAbcde.py
@@ -1,4 +1,23 @@
+def sequence(d):
+   c, l = 0, 1
+   while c < d - 1:
+     if l%2:
+       yield l
+       c += 1
+     l += 1
 
+def make_pattern(d):
+   t, b, r = f'{"A"*(d-1)}+{"B"*(d-1)}', f'{"C"*(d-1)}+{"D"*(d-1)}', list(sequence(d))
+   body = '\n'.join(f'{"A"*((r[-1]-i)//2)}+{"E"*i}+{"B"*((r[-1]-i)//2)}' for i in r[:-1]) + \
+     f'\n+{"E"*r[-1]}+\n'+'\n'.join(f'{"C"*((r[-1]-i)//2)}+{"E"*i}+{"D"*((r[-1]-i)//2)}' for i in r[:-1][::-1])
+   return f'{t}\n{body}\n{b}'
+
+def diamond(d):
+  return {1:lambda _:'+', 2:lambda _:'A+B\n+E+\nC+D'}.get(d, make_pattern)(d)
+
+n = int(input("Input Number : "))
+print("")
+print(diamond(n))
 
 print("")
 print("MENU".center(80, "—"))
